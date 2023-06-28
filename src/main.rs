d85705510d9c3311c3a1b27221b067d086b2ba8e
Patrick Peng <44712940+tenchipsofsalt@users.noreply.github.com>
2023-06-28 10:38:05 -0400

    basic batching and loops

--- src/main.rs
+++ src/main.rs
@@ -22,28 +22,37 @@ fn main() {
     // print_image(&mnist.train_data[5], mnist.train_labels[5]);
     env::set_var("RUST_BACKTRACE", "1");
 
-    let cuda = Cuda::cudnn_is_available();
-    println!("CUDA status: {cuda}. Is your GPU on?");
+    let cudnn = Cuda::cudnn_is_available();
+    Cuda::set_user_enabled_cudnn(true);
+    println!("CUDNN status: {cudnn}. Is your GPU on?");
+    let dev = tch::Device::cuda_if_available();
 
     // mnist
     let m = vision::mnist::load_dir("data").unwrap();
-    let train_x = m.train_images;
-    let train_y = m.train_labels;
-    let test_x = m.test_images;
-    let test_y = m.test_labels;
+    let train_x = m.train_images.to(dev);
+    let train_y = m.train_labels.to(dev);
+    let test_x = m.test_images.to(dev);
+    let test_y = m.test_labels.to(dev);
 
     // init weights
     let mut w = init_weights_uniform();
+    let opt = (tch::Kind::Int, dev);
 
-    'train: for i in 0..STEPS {
-        // compute images x weights in batches
+    for i in 0..STEPS {
+        // make batches
+        let batch_permutation = Tensor::randperm(train_x.size()[0], opt);
 
         // loop over n iterations
-        // permute up to size
-        // in iterations of batch size, slice
-        // get idxs from train by using index_select
-        // forward step 
-        // update weights
+        for i in 0..(train_x.size()[0] / BATCH_SIZE) {
+            let batch_idxs = Tensor::arange_start(i * BATCH_SIZE, (i + 1) * BATCH_SIZE, opt);
+            let train_idxs = batch_permutation.index_select(0, &batch_idxs);
+            let batch_x = train_x.index_select(0, &train_idxs);
+            let batch_y = train_y.index_select(0, &train_idxs);
+            // get idxs from train by using index_select
+            // forward step 
+            // update weights
+
+        }
 
         // epoch stats
     }
@@ -63,9 +72,4 @@ fn init_weights_uniform() -> Tensor {
     let tempsor = tempsor.reshape(&out_shape).set_requires_grad(true);
     println!("Output weight tensor shape: {:?}", out_shape);
     tempsor
-}
-
-fn permute(nums: i64) -> Tensor {
-    let idxs = Tensor::randint(nums, &[nums]);
-    idxs
 }
