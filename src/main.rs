39a72e32d7594f916878dc7cbc8061657f6fe691
Patrick Peng <44712940+tenchipsofsalt@users.noreply.github.com>
2023-06-28 11:36:32 -0400

    need to figure out how to make dynamically sized arrays

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,12 @@ use tch::{Tensor, Cuda, vision};
 use std::env;
 
 // Hyperparams
-const CLASSES: usize = 10;
+const N_CLASSES: usize = 10;
 const INPUT_SIZE: usize = 28 * 28;
 const STEPS: u32 = 200;
 const LR: f32 = 0.1;
 const BATCH_SIZE: i64 = 200;
 
-
-
-const WEIGHTS_UNROLLED_SIZE: usize = (INPUT_SIZE + 1) * CLASSES;
 
 fn main() {
 
@@ -35,21 +32,25 @@ fn main() {
     let test_y = m.test_labels.to(dev);
 
     // init weights
-    let mut w = init_weights_uniform();
+    let mut w1 = init_uniform(INPUT_SIZE, N_CLASSES);
+    let mut b1 = init_uniform(1, 1);
     let opt = (tch::Kind::Int, dev);
 
-    for i in 0..STEPS {
+    for epoch in 0..STEPS {
         // make batches
         let batch_permutation = Tensor::randperm(train_x.size()[0], opt);
 
         // loop over n iterations
-        for i in 0..(train_x.size()[0] / BATCH_SIZE) {
-            let batch_idxs = Tensor::arange_start(i * BATCH_SIZE, (i + 1) * BATCH_SIZE, opt);
+        for batch_no in 0..(train_x.size()[0] / BATCH_SIZE) {
+            // get batch indices and index into train data
+            let batch_idxs = Tensor::arange_start(batch_no * BATCH_SIZE, (batch_no + 1) * BATCH_SIZE, opt);
             let train_idxs = batch_permutation.index_select(0, &batch_idxs);
             let batch_x = train_x.index_select(0, &train_idxs);
             let batch_y = train_y.index_select(0, &train_idxs);
-            // get idxs from train by using index_select
-            // forward step 
+
+            // forward step
+            let out = batch_x.matmul(&w1) + b1;
+
             // update weights
 
         }
@@ -58,17 +59,17 @@ fn main() {
     }
 }
 
-fn init_weights_uniform() -> Tensor {
+fn init_uniform(dim1: usize, dim2: usize) -> Tensor {
     let mut rng = rand::thread_rng();
     let dist = Uniform::new_inclusive(0.0, 1.0);
 
-    let mut weights: [f64; WEIGHTS_UNROLLED_SIZE] = [0.0; WEIGHTS_UNROLLED_SIZE];
+    let mut weights: [f64; dim1 * dim2] = [0.0; dim1 * dim2];
 
     for weight in weights.iter_mut() {
         *weight = dist.sample(&mut rng);
     }
     let tempsor = Tensor::from_slice(&weights);
-    let out_shape: Vec<i64> = vec![(INPUT_SIZE + 1) as i64, CLASSES as i64];
+    let out_shape: Vec<i64> = vec![dim1 as i64, dim2 as i64];
     let tempsor = tempsor.reshape(&out_shape).set_requires_grad(true);
     println!("Output weight tensor shape: {:?}", out_shape);
     tempsor
